58cf7e8000c7a9ea3f8bebc55434af4f24bd27fb
cbmdk <cbm@cbm.dk>
2020-10-09 14:46:37 +0200

    no message

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                cryptodyst
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
+
